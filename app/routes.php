cdf4c85e001628eba924a3416c2f299a493a23be
Abhishek <ia.malhotra@gmail.com>
2013-11-06 11:12:18 +0530

    bootstrap libs added

--- app/routes.php
+++ app/routes.php
@@ -13,5 +13,5 @@
 
 Route::get('/', function()
 {
-	return View::make('hello1');
+	return View::make('home');
 });
---
+++ app/views/home.blade.php
@@ -0,0 +1,6 @@
+@extends('layout')
+
+@section('content')
+    Users!
+    <h2>Todo list</h2>
+@stop
