6cc508eada773a0b2130aed46712d4ecf48fc79e
Abhishek <ia.malhotra@gmail.com>
2013-11-06 12:51:06 +0530

    added layout.blade.php

---
+++ app/views/layout.blade.php
@@ -0,0 +1,18 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="custom accounting">
+    <meta name="author" content="mindfire">
+    <title>Accounting</title>
+    {{ basset_stylesheets('application') }}
+    {{ basset_javascripts('application') }}
+</head>
+<body>
+    <h1>Helo</h1>
+
+    @yield('content')
+</body>
+</html>
